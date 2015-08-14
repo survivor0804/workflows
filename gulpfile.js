b355234f49e3fb2f46b065cee37dcd0d1b97c59b
survivor0804 <nino@queen.core-dev.com>
2015-08-13 21:11:41 -0400

    Added Coffee task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-	gutil.log('Workflows are awesome');
+var coffeeSources = ['components/coffee/*.coffee'],
+	coffeeDestination = 'components/scripts';
+	
+gulp.task('coffee', function() {
+	gulp.src(coffeeSources)
+	.pipe(coffee({
+		bare: true
+	}))
+	.on('error', gutil.log)
+	.pipe(gulp.dest(coffeeDestination));
 });

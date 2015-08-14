fcb9681a79bdfa69383c91a9199b096b993b0497
survivor0804 <nino@queen.core-dev.com>
2015-08-13 21:54:40 -0400

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
-	concat = require('gulp-concat');
+	concat = require('gulp-concat'),
+	browserify = require('gulp-browserify');
 
 var coffeeSources = ['components/coffee/*.coffee'],
 	coffeeDestination = 'components/scripts',
@@ -20,5 +21,6 @@ gulp.task('coffee', function() {
 gulp.task('js', function() {
 	gulp.src(jsSources)
 	.pipe(concat('script.js'))
+	.pipe(browserify())
 	.pipe(gulp.dest(jsDestination));
 });

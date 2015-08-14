ea60d37efa5d30dec7d12a21cbf9dd8e9d5160f9
survivor0804 <nino@queen.core-dev.com>
2015-08-13 22:13:10 -0400

    Added compass task

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	concat = require('gulp-concat'),
-	browserify = require('gulp-browserify');
+	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass');
 
 var coffeeSources = ['components/coffee/*.coffee'],
 	coffeeDestination = 'components/scripts',
 	jsSources = ['components/scripts/*.js'],
-	jsDestination = 'builds/development/js';
+	jsDestination = 'builds/development/js',
+	sassSources = ['components/sass/style.scss'],
+	sassDestination = 'builds/development/css';
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -23,4 +26,15 @@ gulp.task('js', function() {
 	.pipe(concat('script.js'))
 	.pipe(browserify())
 	.pipe(gulp.dest(jsDestination));
+});
+
+gulp.task('compass', function() {
+	gulp.src(sassSources)
+	.pipe(compass({
+		sass: 'components/sass',
+		image: 'builds/development/images',
+		style: 'expanded'
+	}))
+	.on('error', gutil.log)
+	.pipe(gulp.dest(sassDestination));
 });
